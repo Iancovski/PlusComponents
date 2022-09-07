1ebda0e50341314c881159742c6f28cd9b631fd0
Iancovski <46848500+Iancovski@users.noreply.github.com>
2022-09-07 14:34:47 -0300

    First version of PEdit and PLabel

--- source/PEdit.pas
+++ source/PEdit.pas
@@ -3,18 +3,40 @@ unit PEdit;
 interface
 
 uses
-  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls;
+  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Winapi.Messages, Winapi.Windows, PLabel;
+
+type
+  TLabelPosition = (lpAbove, lpBelow, lpLeft, lpRight);
 
 type
   TPEdit = class(TEdit)
+
   private
-    { Private declarations }
+    FEditLabel: TPLabel;
+    FLabelPosition: TLabelPosition;
+    FLabelSpacing: Integer;
+
   protected
-    { Protected declarations }
+    function AdjustedAlignment(RightToLeftAlignment: Boolean; Alignment: TAlignment): TAlignment;
+    procedure SetParent(AParent: TWinControl); override;
+    procedure Notification(AComponent: TComponent; Operation: TOperation); override;
+    procedure SetName(const Value: TComponentName); override;
+    procedure CMVisiblechanged(var Message: TMessage); message CM_VISIBLECHANGED;
+    procedure CMEnabledchanged(var Message: TMessage); message CM_ENABLEDCHANGED;
+    procedure CMBidimodechanged(var Message: TMessage); message CM_BIDIMODECHANGED;
+
   public
-    { Public declarations }
+    constructor Create(AOwner: TComponent); override;
+    procedure SetBounds(ALeft: Integer; ATop: Integer; AWidth: Integer; AHeight: Integer); override;
+    procedure SetLabelPosition(const Value: TLabelPosition);
+    procedure SetLabelSpacing(const Value: Integer);
+    procedure SetupInternalLabel;
+
   published
-    { Published declarations }
+    property EditLabel: TPLabel read FEditLabel;
+    property LabelPosition: TLabelPosition read FLabelPosition write SetLabelPosition default lpAbove;
+    property LabelSpacing: Integer read FLabelSpacing write SetLabelSpacing default 3;
   end;
 
 procedure Register;
@@ -24,6 +46,150 @@ implementation
 procedure Register;
 begin
   RegisterComponents('Plus Components', [TPEdit]);
+end;
+
+{ TPEdit }
+
+function TPEdit.AdjustedAlignment(RightToLeftAlignment: Boolean; Alignment: TAlignment): TAlignment;
+begin
+  Result := Alignment;
+
+  if RightToLeftAlignment then begin
+    case Result of
+      taLeftJustify:
+        Result := taRightJustify;
+      taRightJustify:
+        Result := taLeftJustify;
+    end;
+  end;
+end;
+
+procedure TPEdit.CMBidimodechanged(var Message: TMessage);
+begin
+  inherited;
+
+  if FEditLabel <> nil then
+    FEditLabel.BiDiMode := BiDiMode;
+end;
+
+procedure TPEdit.CMEnabledchanged(var Message: TMessage);
+begin
+  inherited;
+
+  if FEditLabel <> nil then
+    FEditLabel.Enabled := Enabled;
+end;
+
+procedure TPEdit.CMVisiblechanged(var Message: TMessage);
+begin
+  inherited;
+
+  if FEditLabel <> nil then
+    FEditLabel.Visible := Visible;
+end;
+
+constructor TPEdit.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  FLabelPosition := lpAbove;
+  FLabelSpacing := 3;
+  SetupInternalLabel;
+end;
+
+procedure TPEdit.Notification(AComponent: TComponent; Operation: TOperation);
+begin
+  inherited Notification(AComponent, Operation);
+
+  if (AComponent = FEditLabel) and (Operation = opRemove) then
+    FEditLabel := nil;
+end;
+
+procedure TPEdit.SetBounds(ALeft, ATop, AWidth, AHeight: Integer);
+begin
+  inherited SetBounds(ALeft, ATop, AWidth, AHeight);
+
+  SetLabelPosition(FLabelPosition);
+end;
+
+procedure TPEdit.SetLabelPosition(const Value: TLabelPosition);
+var
+  P: TPoint;
+begin
+  if FEditLabel = nil then
+    Exit;
+
+  FLabelPosition := Value;
+
+  case Value of
+    lpAbove:
+      case AdjustedAlignment(UseRightToLeftAlignment, Alignment) of
+        taLeftJustify:
+          P := Point(Left, Top - FEditLabel.Height - FLabelSpacing);
+        taRightJustify:
+          P := Point(Left + Width - FEditLabel.Width, Top - FEditLabel.Height - FLabelSpacing);
+        taCenter:
+          P := Point(Left + (Width - FEditLabel.Width) div 2, Top - FEditLabel.Height - FLabelSpacing);
+      end;
+    lpBelow:
+      case AdjustedAlignment(UseRightToLeftAlignment, Alignment) of
+        taLeftJustify:
+          P := Point(Left, Top + Height + FLabelSpacing);
+        taRightJustify:
+          P := Point(Left + Width - FEditLabel.Width, Top + Height + FLabelSpacing);
+        taCenter:
+          P := Point(Left + (Width - FEditLabel.Width) div 2, Top + Height + FLabelSpacing);
+      end;
+    lpLeft:
+      P := Point(Left - FEditLabel.Width - FLabelSpacing, Top + ((Height - FEditLabel.Height) div 2));
+    lpRight:
+      P := Point(Left + Width + FLabelSpacing, Top + ((Height - FEditLabel.Height) div 2));
+  end;
+
+  FEditLabel.SetBounds(P.x, P.y, FEditLabel.Width, FEditLabel.Height);
+end;
+
+procedure TPEdit.SetLabelSpacing(const Value: Integer);
+begin
+  FLabelSpacing := Value;
+  SetLabelPosition(FLabelPosition);
+end;
+
+procedure TPEdit.SetName(const Value: TComponentName);
+var
+  LClearText: Boolean;
+begin
+  if (csDesigning in ComponentState) and (FEditLabel <> nil) and
+    ((FEditLabel.GetTextLen = 0) or (CompareText(FEditLabel.Caption, Name) = 0)) then
+    FEditLabel.Caption := Value;
+
+  LClearText := (csDesigning in ComponentState) and (Text = '');
+
+  inherited SetName(Value);
+
+  if LClearText then
+    Text := '';
+end;
+
+procedure TPEdit.SetParent(AParent: TWinControl);
+begin
+  inherited SetParent(AParent);
+
+  if FEditLabel = nil then
+    Exit;
+
+  FEditLabel.Parent := AParent;
+  FEditLabel.Visible := True;
+end;
+
+procedure TPEdit.SetupInternalLabel;
+begin
+  if Assigned(FEditLabel) then
+    Exit;
+
+  FEditLabel := TPLabel.Create(Self);
+  FEditLabel.FreeNotification(Self);
+  FEditLabel.FocusControl := Self;
 end;
 
 end.
--- source/PLabel.pas
+++ source/PLabel.pas
@@ -3,27 +3,123 @@ unit PLabel;
 interface
 
 uses
-  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls;
+  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls, Vcl.Dialogs;
+
+type
+  TLabelAlignment = (laTop, laLeft, laRight, laCenter);
 
 type
   TPLabel = class(TLabel)
+
   private
-    { Private declarations }
+    FLabelAlignment: TLabelAlignment;
+    FMasterLabel: TPLabel;
+    procedure SetMasterLabel(const Value: TPLabel);
+    procedure SetLabelAlignment(const Value: TLabelAlignment);
+
   protected
-    { Protected declarations }
+    SettingMasterAlignment: Boolean;
+    procedure AdjustBounds; override;
+
   public
-    { Public declarations }
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure SetBounds(ALeft: Integer; ATop: Integer; AWidth: Integer; AHeight: Integer); override;
+
   published
-    { Published declarations }
+    property MasterAlignment: TLabelAlignment read FLabelAlignment write SetLabelAlignment default laLeft;
+    property MasterLabel: TPLabel read FMasterLabel write SetMasterLabel;
   end;
 
 procedure Register;
 
 implementation
 
+uses
+  PEdit;
+
 procedure Register;
 begin
   RegisterComponents('Plus Components', [TPLabel]);
+end;
+
+{ TPLabel }
+
+procedure TPLabel.AdjustBounds;
+begin
+  inherited AdjustBounds;
+
+  if Owner is TPEdit then
+    TPEdit(Owner).SetLabelPosition(TPEdit(Owner).LabelPosition);
+
+  if not SettingMasterAlignment then
+    SetLabelAlignment(FLabelAlignment);
+end;
+
+constructor TPLabel.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  if Owner is TPEdit then begin
+    Name := 'SubLabel';
+    SetSubComponent(True);
+    Caption := AOwner.Name;
+  end;
+end;
+
+destructor TPLabel.Destroy;
+var
+  i: Integer;
+begin
+  for i := 0 to Owner.ComponentCount - 1 do begin
+    if (Owner.Components[i] is TPLabel) then begin
+      if (Assigned(TPLabel(Owner.Components[i]).MasterLabel)) and (TPLabel(Owner.Components[i]).MasterLabel = Self) then
+        TPLabel(Owner.Components[i]).MasterLabel := nil;
+    end;
+  end;
+
+  inherited;
+end;
+
+procedure TPLabel.SetBounds(ALeft, ATop, AWidth, AHeight: Integer);
+begin
+  inherited SetBounds(ALeft, ATop, AWidth, AHeight);
+
+  if not SettingMasterAlignment then
+    SetLabelAlignment(FLabelAlignment);
+end;
+
+procedure TPLabel.SetLabelAlignment(const Value: TLabelAlignment);
+begin
+  FLabelAlignment := Value;
+
+  SettingMasterAlignment := True;
+  if Assigned(MasterLabel) then begin
+    case FLabelAlignment of
+      laTop:
+        Self.Top := MasterLabel.Top;
+      laLeft:
+        Self.Left := MasterLabel.Left;
+      laRight:
+        Self.Left := (MasterLabel.Left + MasterLabel.Width) - Self.Width;
+      laCenter:
+        Self.Left := (MasterLabel.Left + MasterLabel.Width div 2) - (Self.Width div 2);
+    end;
+  end;
+  SettingMasterAlignment := False;
+end;
+
+procedure TPLabel.SetMasterLabel(const Value: TPLabel);
+begin
+  if (Value <> nil) and (Self.Owner <> Value.Owner) then begin
+    ShowMessage('It is not allowed to set a MasterLabel of another Owner.');
+    Abort;
+  end;
+
+  FMasterLabel := Value;
+
+  if not SettingMasterAlignment then
+    SetLabelAlignment(FLabelAlignment);
 end;
 
 end.
